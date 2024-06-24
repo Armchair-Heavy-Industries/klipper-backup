bfd38c5b6856d13146d42e35b651e492b9678f07
Bea Nance <bwnance@gmail.com>
2024-06-24 19:09:18 -0400

    don't backup systemd

--- scripts/initial_backup.sh
+++ scripts/initial_backup.sh
@@ -5,7 +5,7 @@ git init
 git remote add origin https://github.com/$GHUSERNAME/$REPONAME.git
 
 
-printf "logs/\ncomms/\ncerts/\ngcodes/\nbackup/\nmoonraker.asvc\nmisc/usb.ids\n.moonraker.uuid\n" > .gitignore
+printf "systemd/\nlogs/\ncomms/\ncerts/\ngcodes/\nbackup/\nmoonraker.asvc\nmisc/usb.ids\n.moonraker.uuid\n" > .gitignore
 git add .
 git commit -m "first backup"
 git push -u origin $BRANCHNAME
