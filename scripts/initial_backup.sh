06765c8bd8b833ec88351b4d5d9dfdb3cf3d71f2
Bea Nance <bwnance@gmail.com>
2024-06-26 12:19:21 -0400

    add git branch -m

--- scripts/initial_backup.sh
+++ scripts/initial_backup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 gh repo create $REPONAME --private
 cd /home/$USERNAME/printer_data
 git init
+git branch -m $BRANCHNAME
 git remote add origin https://github.com/$GHUSERNAME/$REPONAME.git
 
-
-printf "systemd/\nlogs/\ncomms/\ncerts/\ngcodes/\nbackup/\nmoonraker.asvc\nmisc/usb.ids\n.moonraker.uuid\n" > .gitignore
+printf "systemd/\nlogs/\ncomms/\ncerts/\ngcodes/\nbackup/\nmoonraker.asvc\nmisc/usb.ids\n.moonraker.uuid\n.theme/\n" > .gitignore
 git add .
 git commit -m "first backup"
 git push -u origin $BRANCHNAME
